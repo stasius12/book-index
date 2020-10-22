3885f26fa2de97078b084f46dc6d0af2deabfab2
Marek Piechula <mpiechula@blees.co>
2020-10-22 22:26:02 +0200

    black

--- book_index/__main__.py
+++ book_index/__main__.py
@@ -2,9 +2,9 @@
 import argparse
 from book_index.index import do_it
 
-parser = argparse.ArgumentParser(description='create a index of words')
-parser.add_argument('FILE')
-parser.add_argument('--format', default='text')
+parser = argparse.ArgumentParser(description="create a index of words")
+parser.add_argument("FILE")
+parser.add_argument("--format", default="text")
 
 args = parser.parse_args()
 with open(args.FILE) as stream:
--- book_index/index.py
+++ book_index/index.py
@@ -1,7 +1,8 @@
 from io import TextIOBase
 
-def do_it(stream: TextIOBase, format: str ='text'):
+
+def do_it(stream: TextIOBase, format: str = "text"):
     txt = stream.readlines()
-    print('THIS IS AN EXAMPLE!')
-    print('your text has', len(txt), 'lines')
-    print('format is', format)
+    print("THIS IS AN EXAMPLE!")
+    print("your text has", len(txt), "lines")
+    print("format is", format)
