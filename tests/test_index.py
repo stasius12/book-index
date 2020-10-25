ec06e122932157a5683e8c1b0fb4f91a2e2261c4
Michał Klich <michal@klichx.dev>
2020-10-25 15:45:35 +0100

    Make test more real

--- tests/test_index.py
+++ tests/test_index.py
@@ -5,7 +5,4 @@ from book_index.index import do_it
 
 def test_do_it():
     stream = StringIO("Foo Foo Foo")
-    do_it(stream)
-
-    x = 1 + 1
-    assert x == 2
+    assert do_it(stream) == "Foo    1,1; 1,5; 1,9"
