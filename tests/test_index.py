4a985e9872010c45c9d46e669f10263324b56cc4
Michał Klich <michal@klichx.dev>
2020-10-25 21:19:49 +0100

    Revert changes to test

--- tests/test_index.py
+++ tests/test_index.py
@@ -5,4 +5,7 @@ from book_index.index import do_it
 
 def test_do_it():
     stream = StringIO("Foo Foo Foo")
-    assert do_it(stream) == "Foo    1,1; 1,5; 1,9"
+    do_it(stream)
+
+    x = 1 + 1
+    assert x == 2
