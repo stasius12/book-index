fd7bd51f3c3f062d36071ed40da45e31997246d5
QBack <qback.contact@gmail.com>
2020-10-29 18:31:28 +0100

    renamed format to prevent name shadowing

--- book_index/index.py
+++ book_index/index.py
@@ -1,8 +1,8 @@
 from io import TextIOBase
 
 
-def do_it(stream: TextIOBase, format: str = "text"):
+def do_it(stream: TextIOBase, format_: str = "text"):
     txt = stream.readlines()
     print("THIS IS AN EXAMPLE!")
     print("your text has", len(txt), "lines")
-    print("format is", format)
+    print("format is", format_)
