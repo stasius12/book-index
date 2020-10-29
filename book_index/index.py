021eae493840cdb0863d50996d1df36ca6c9cd97
Stanisław Stępak <stanislaw@ulam.io>
2020-10-29 19:21:30 +0100

    first lines

--- book_index/index.py
+++ book_index/index.py
@@ -1,4 +1,9 @@
+from collections import defaultdict
 from io import TextIOBase
+from typing import DefaultDict
+
+
+SEPERATOR_LIST = [" ", ".", "\n", "\0", ""]
 
 
 def do_it(stream: TextIOBase, format_: str = "text"):
@@ -6,3 +11,31 @@ def do_it(stream: TextIOBase, format_: str = "text"):
     print("THIS IS AN EXAMPLE!")
     print("your text has", len(txt), "lines")
     print("format is", format_)
+    return find_all_words_in_text(txt)
+
+
+def find_all_words_in_text(all_lines: list) -> DefaultDict:
+    """
+        result: {
+            "ala": [(1,1), (2, 2)],
+            ...
+        }
+    """
+    result = defaultdict(list)
+    start_idx = 1
+
+    for row_number, line in enumerate(all_lines, 1):
+        for col_number, char_ in enumerate(line, 1):
+            if char_ in SEPERATOR_LIST:
+                word = line[start_idx - 1 : col_number - 1]
+                result[word].append((row_number, start_idx))
+                start_idx = col_number + 1
+        # result[word].append((row_number, start_idx))
+
+    return result
+
+
+def parse_line(line: str) -> dict:
+    for col_number, char_ in enumerate(line):
+        if char_ in SEPERATOR_LIST:
+            pass
--- tests/test_index.py
+++ tests/test_index.py
@@ -1,6 +1,8 @@
 from io import StringIO
 
-from book_index.index import do_it
+import pytest
+
+from book_index.index import do_it, find_all_words_in_text, parse_line
 
 
 def test_do_it():
@@ -9,3 +11,17 @@ def test_do_it():
 
     x = 1 + 1
     assert x == 2
+
+
+@pytest.mark.parametrize(
+    "txt,result", [(["ala ma kota"], {"ala": [(1, 1)], "ma": [(1, 5)], "kota": [(1, 8)]})]
+)
+def test_find_all_words_in_text(txt: list, result: dict):
+    assert find_all_words_in_text(txt) == result
+
+
+@pytest.mark.parametrize(
+    "line,result", [(["ala ma kota"], {"ala": [(1, 1)], "ma": [(1, 4)], "kota": [(1, 8)]})]
+)
+def test_parse_line(line: str, result: dict):
+    assert parse_line(line) == result
